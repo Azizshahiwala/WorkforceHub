bc19aa7ca38cdc8a42f8fab22de7ddf08be7b61d
Azizshahiwala <shahiwalaaziz8@gmail.com>
2025-12-17 19:45:13 +0530

    commit

--- src/HR/Navbar.jsx
+++ src/HR/Navbar.jsx
@@ -10,3 +10,11 @@ function Navbar() {
 }
 
 export default Navbar;
+//Used in local storage
+// const va = [
+
+//   {id: 0, name: 'Aziz'}
+//   {id: 1, name: 'Azi'}
+//   {id: 2, name: 'Az'}
+//   {id: 3, name: 'A'}
+// ];
