b7df64821b08819d517e8b63378bf093c1a6af9c
Azizshahiwala <shahiwalaaziz8@gmail.com>
2025-12-17 19:34:31 +0530

    commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 
-<<<<<<< HEAD
-export default function Dashboard() {
-=======
 function Dashboard() {
->>>>>>> dcfbc085f8706a91d880dd84c378a1c19fcbfae8
   return (
     <div>Dashboard</div>
   )
 }
-<<<<<<< HEAD
-=======
 
-export default Dashboard
->>>>>>> dcfbc085f8706a91d880dd84c378a1c19fcbfae8
+export default Dashboard;
