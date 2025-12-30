a8bfe4e0fd46a96fcaf4d752bead06373fec9154
w_vivek <wvivek.181@gmail.com>
2025-12-30 19:24:04 +0530

    commit

--- src/pages/EmployeeDashboard.jsx
+++ src/pages/EmployeeDashboard.jsx
@@ -1,27 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from 'react'
 
 function EmployeeDashboard() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const savedUser = localStorage.getItem("loggedEmployee");
-    if (savedUser) {
-      setUser(JSON.parse(savedUser));
-    }
-  }, []);
-
-  if (!user) {
-    return <h1>No user logged in</h1>;
-  }
-
   return (
-    <div>
-      <h1>Welcome {user.name}</h1>
-      <p>Role: {user.role}</p>
-      <p>Gender: {user.gender}</p>
-      <p>Department: {user.department}</p>
-    </div>
-  );
+    <div>EmployeeDashboard</div>
+  )
 }
 
-export default EmployeeDashboard;
+export default EmployeeDashboard
