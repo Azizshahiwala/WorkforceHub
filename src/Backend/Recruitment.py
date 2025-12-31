28b0508450ae745b7ba5109a75d7250980fca214
Azizshahiwala <shahiwalaaziz8@gmail.com>
2025-12-31 13:00:51 +0530

    Added recruitment.py (base template) And renamed folder for structure

---
+++ src/Backend/Recruitment.py
@@ -0,0 +1,6 @@
+#This file is entry point for resume uploading.
+#This step creates separate react file for resume uploading feature.
+#Then it is sent to Recruitement.jsx
+#when approved, it is stored in db. Once approved, registered user cannot access this portal.
+#If rejected, entry is deleted and guest can try to upload again.
+
