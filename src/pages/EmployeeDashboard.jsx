ae9f7ba58edd6050e224d30e16a2cbc71deed324
w_vivek <wvivek.181@gmail.com>
2026-01-01 10:22:19 +0530

    commit

--- src/pages/EmployeeDashboard.jsx
+++ src/pages/EmployeeDashboard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function EmployeeDashboard() {
+export default function EmployeeDashboard() {
+  const [employee, setEmployee] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const empData = localStorage.getItem("employee");
+
+    if (!empData) {
+      navigate("/");
+    } else {
+      setEmployee(JSON.parse(empData));
+    }
+  }, [navigate]);
+
+  if (!employee) return <h2>Loading...</h2>;
+
   return (
-    <div>EmployeeDashboard</div>
-  )
+    <div>
+      <h1>Welcome, {employee.role}!</h1>
+    </div>
+  );
 }
-
-export default EmployeeDashboard
