bdd102e3ba2331c27254ebe26fee1c7c1db9927c
w_vivek <wvivek.181@gmail.com>
2026-03-16 10:13:10 +0530

    Updated the Employee Dashboard to temporarily

--- src/pages/Employees/EmployeeDashboard.jsx
+++ src/pages/Employees/EmployeeDashboard.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from "react";
-import MessageBox from "../../Misc/MessageBox";
+import "../../styles/Employees/AssignedTaskByHR.css";
 function EmployeeDashboard() {
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
   const [employee, setEmployee] = useState(() => {
     const savedSession = localStorage.getItem("MySession");
     return savedSession ? JSON.parse(savedSession) : null;
   });
+
+  const [projectData, setProjectData] = useState(null);
+
+  useEffect(() => {
+    const stored = JSON.parse(localStorage.getItem("projectData"));
+    if (stored) {
+      setProjectData(stored);
+    }
+  }, []);
 
   if (!employee) {
     return (
@@ -14,10 +24,32 @@ function EmployeeDashboard() {
       </div>
     );
   }
+
+  if (!projectData) {
+    return <h3 className="employee-no-task">No task assigned yet</h3>;
+  }
   
   return (
-    <div>
-      <h1>Welcome, {employee.name}!</h1>
+     <div className="employee-view-page">
+      <h1>Hello {employee.name};</h1>
+      <h1 className="employee-view-title">Assigned Task</h1>
+      <div className="employee-assigned-card">
+        <h2>Assigned Project</h2>
+        <p>
+          <strong>Project Name:</strong> {projectData.projectName}
+        </p>
+        <p>
+          <strong>Company Name:</strong> {projectData.companyName}
+        </p>
+        <h3>Assigned Team Members</h3>
+        <ul>
+          {projectData.employees.map((emp) => (
+            <li key={emp.uid || emp.id}>
+              {emp.name} - {emp.role}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
