a45b67156803b878e0581897edfcff0ee2f277fd
Azizshahiwala <143518759+Azizshahiwala@users.noreply.github.com>
2026-03-19 15:14:37 +0530

    Enhance RegisterForm with role base salary and validation

--- src/Login/RegisterForm.jsx
+++ src/Login/RegisterForm.jsx
@@ -2,18 +2,17 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./RegisterForm.css";
 import { Link } from "react-router-dom";
-const UploadToUrl = "http://localhost:5000/api/RegisterForm/applications/upload";
+import MessageBox from "../Misc/MessageBox";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const roles = [
-  { id: "Employee", label: "Employees"},
-  { id: "Interviewer", label: "Interviewer"},
-  { id: "Sales manager", label: "Sales manager"},
-  { id: "Designer", label: "Designer"},
-  { id: "Developer", label: "Developer"},
-  { id: "Marketing", label: "Marketing"},
-  { id: "Finance", label: "Finance"},
-  { id: "Support", label: "Support"},
-  { id: "Tester", label: "Tester"},
-  { id: "Intern", label: "Intern"},
+  { id: "Sales manager", label: "Sales manager",BaseSalary:62000},
+  { id: "Designer", label: "Designer", BaseSalary:45000},
+  { id: "Developer", label: "Developer", BaseSalary:55000},
+  { id: "Marketing", label: "Marketing", BaseSalary:48000},
+  { id: "Finance", label: "Finance", BaseSalary:58000},
+  { id: "Support", label: "Support", BaseSalary:38000},
+  { id: "Tester", label: "Tester", BaseSalary:42000},
+  { id: "Intern", label: "Intern", BaseSalary:18000},
 ];
 {/*Roles you cannot apply for*/}
 const rolesYouCannotApplyFor = ["admin", "hr", "ceo"];
@@ -21,12 +20,27 @@ const rolesYouCannotApplyFor = ["admin", "hr", "ceo"];
 export default function RegisterForm() {
   const navigate = useNavigate();
   const [file, setFile] = useState(null)
-  const [selectedRole, setSelectedRole] = useState("");
+  const [selectedRole, setSelectedRole] = useState("Intern");
+  const [selectedSal,setselectedSal] = useState(0);
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [gender,setGender] = useState("");
-  const [personExperience, setpersonExperience] = useState("")
-  const [name, setName] = useState("")
+  const [personExperience, setpersonExperience] = useState("");
+  const [name, setName] = useState("");
+  const [message,setMessage] = useState(null);
+  const handleRoleChange = (e) => {
+  const roleId = e.target.value;
+  setSelectedRole(roleId);
+  
+  //Now find the object which has r.id
+  const roleObj = roles.find(r => r.id === roleId);
+  if (roleObj) {
+    setselectedSal(roleObj.BaseSalary);
+  } else {
+    setselectedSal(0);
+  }
+};
+
   const handleFileChange = (e) => {
     if(e.target.files.length == 1){
       setFile(e.target.files[0]);
@@ -35,12 +49,13 @@ export default function RegisterForm() {
   const handleRegister = async (e) => {
   e.preventDefault();
   if (!selectedRole || !email || !phoneNumber || !file || !gender || !personExperience || !name) {
-      alert("Please fill all required fields correctly.");
+      setMessage({type:"Info",text:"Please fill all required fields correctly."});
       return;
     }
 
-    if (phoneNumber.length !== 13) {
-        alert("Phone number must be of 12 digits.");
+    let countrycodesyntax = (phoneNumber.includes("+") && phoneNumber.charAt(0) == "+");
+    if (phoneNumber.length !== 13 && !countrycodesyntax) {
+        setMessage({type:"Info",text:"Phone number must be of this syntax: +01xxxxxxxxxx"});
         return;
     }
   try {
@@ -52,9 +67,10 @@ export default function RegisterForm() {
     formData.append('gender', gender);
     formData.append('name', name);
     formData.append('personExperience', personExperience);
-
-    const response = await fetch(UploadToUrl, {
+    formData.append('selectedSal',selectedSal)
+    const response = await fetch(`${API_BASE_URL}/RegisterForm/applications/upload`, {
       method: "POST",
+      credentials: "include",
       body: formData,
       });
       const data = await response.json();
@@ -64,16 +80,18 @@ export default function RegisterForm() {
       navigate("/"); // Redirect to root which will now show Login
     }
     else{
-      alert("Registration failed: " + data.message);
+      setMessage({type:"Error",text:"Registration failed: " + data.message});
       return;
     }
     
   } catch (error) {
-    console.error("❌ Registration error:", error);
+    console.error("Registration error:", error);
+    setMessage({type:"Error",text:"Registration error:", error});
   }
 }   
   return (
     <div className="login-wrapper">
+      <MessageBox message={message} onClose={() => setMessage(null)}/>
       <form className="Register-form" onSubmit={handleRegister}>
         <div className="SensitiveInfo">
           <p>Enter your name: 
@@ -93,15 +111,15 @@ export default function RegisterForm() {
         </div>
 
         <div className="roles">
-            <select value={selectedRole} onChange={(e) => setSelectedRole(e.target.value)}>
-              <option value="">Select Role</option>
-              {roles.map((role) => (
-                <option key={role.id} value={role.id}>
-                  {role.label}
-                </option>
-              ))}
-            </select>
-        </div>
+    <select value={selectedRole} onChange={handleRoleChange}>
+      <option value="">Select Role</option>
+      {roles.map((role) => (
+        <option key={role.id} value={role.id}>
+          {role.label} (Base Salary: ₹{role.BaseSalary})
+        </option>
+      ))}
+    </select>
+</div>
 
         <div className="ResumeUploadArea">
           <input type="file" accept=".pdf" onChange={handleFileChange} />
@@ -115,6 +133,7 @@ export default function RegisterForm() {
           Submit Application
         </button>
         <Link to="/">Already have an account? Click here</Link>
+        
       </form>
     </div>
   );
