4aacfb22ad37a047ca01ce8db306a607702e449b
w_vivek <wvivek.181@gmail.com>
2026-04-17 19:03:59 +0530

    ..

--- src/pages/Employees/EmployeeDashboard.jsx
+++ src/pages/Employees/EmployeeDashboard.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import "../../styles/Employees/AssignedTaskByHR.css";
 function EmployeeDashboard() {
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
   const [employee, setEmployee] = useState(() => {
     const savedSession = localStorage.getItem("MySession");
     return savedSession ? JSON.parse(savedSession) : null;
@@ -19,6 +18,10 @@ function EmployeeDashboard() {
   return (
      <div className="employee-view-page">
       <h1>Hello {employee.name};</h1>
+      <h3>Employee ID: {employee.employeeId}</h3>
+      <h2>Welcome to your dashboard!</h2>
+      <h3>Role:{employee.role}</h3>
+      <h3>Email: {employee.email}</h3>
     </div>
   );
 }
